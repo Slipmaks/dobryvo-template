4fcf345effad008ad0889a6159945cab82d468fd
Slipmaks <slipmaks@gmail.com>
2022-03-25 09:58:05 +0200

    init

---
+++ src/components/Slider.vue
@@ -0,0 +1,41 @@
+<template>
+  <Splide :options="options">
+    <SplideSlide>
+      <img src="../assets/evergeen-souse/Header.png" alt="" />
+    </SplideSlide>
+    <SplideSlide>
+      <img src="../assets/evergeen-souse/Header.png" alt="" />
+    </SplideSlide>
+    <SplideSlide>
+      <img src="../assets/evergeen-souse/Header.png" alt="" />
+    </SplideSlide>
+  </Splide>
+</template>
+<script>
+import "@splidejs/splide/dist/css/splide.min.css";
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+
+export default {
+  components: {
+    Splide,
+    SplideSlide,
+  },
+  setup() {
+    const options = {
+      rewind: true,
+      gap: "1rem",
+      autoplay: true,
+      speed: 3000,
+      arrows: false,
+      pagination: true,
+    };
+
+    return { options };
+  },
+};
+</script>
+<style>
+.splide__slide img {
+  width: 100%;
+}
+</style>
