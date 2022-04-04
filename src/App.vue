c6a949592295e9e93fddb570e7c5b2ef458168c7
Slipmaks <slipmaks@gmail.com>
2022-04-04 16:13:10 +0300

    updated form

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,14 @@
 </template>
 <script>
 import Slider from "./components/Slider.vue";
-import Form from "./components/Form.vue"
+import Form from "./components/Form.vue";
 
 export default {
   components: {
     Slider,
-    Form
+    Form,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,42 +1,67 @@
 <template>
   <div>
-    <input
-      type="text"
-      placeholder="input text"
-      v-model="typeOfСrop.inputValue"
-      @blur="validate(typeOfСrop)"
-    />
+    <p>Выберете культуру</p>
+    <select v-model="typeOfСrop.inputValue" @blur="validate(typeOfСrop)">
+      <option value="3100">Пшеница ≈ 3100 грн / т</option>
+      <option value="3300">Гречка ≈ 3300 грн / т</option>
+      <option value="2900">Картофель ≈ 2900 грн / т</option>
+    </select>
     <p v-if="typeOfСrop.error.length">{{ typeOfСrop.error }}</p>
   </div>
   <div>
+    <p>Урожайность, ц/га</p>
     <input
       type="text"
-      placeholder="input text"
+      placeholder="20"
       v-model="yieldValue.inputValue"
       @blur="validate(yieldValue)"
     />
     <p v-if="yieldValue.error.length">{{ yieldValue.error }}</p>
   </div>
+  <div>
+    <p>Стоимость хим. удобрениея, грн/га</p>
+    <input
+      type="text"
+      placeholder="3000"
+      v-model="fertilizerPrice.inputValue"
+      @blur="validate(fertilizerPrice)"
+    />
+    <p v-if="fertilizerPrice.error.length">{{ fertilizerPrice.error }}</p>
+  </div>
+  <div>
+    <p>Посевная площадь, га</p>
+    <input
+      type="text"
+      placeholder="10"
+      v-model="sownArea.inputValue"
+      @blur="validate(sownArea)"
+    />
+    <p v-if="sownArea.error.length">{{ sownArea.error }}</p>
+  </div>
 </template>
 <script>
-import { reactive, ref } from "vue";
+import { ref } from "vue";
 
 export default {
   setup() {
-    const typeOfСrop = reactive({ inputValue: "", error: "" });
+    const typeOfСrop = ref({ inputValue: "", error: "" });
     const yieldValue = ref({ inputValue: "", error: "" });
+    const fertilizerPrice = ref({ inputValue: "", error: "" });
+    const sownArea = ref({ inputValue: "", error: "" });
     const stringValidate = new RegExp("[^0-9]");
 
     function validate(objName) {
-      console.log(objName.inputValue);
-      if (objName.inputValue == 0 || objName.inputValue.match(stringValidate) ||objName.inputValue == undefined) {
+      if (
+        objName.inputValue == 0 ||
+        objName.inputValue.match(stringValidate) ||
+        objName.inputValue == undefined
+      ) {
         objName.error = "enter correct the value";
-      } else{
-        objName.error = ""
+      } else {
+        objName.error = "";
       }
-     console.log(objName)
     }
-    return { validate, typeOfСrop, yieldValue };
+    return { validate, typeOfСrop, yieldValue, fertilizerPrice, sownArea };
   },
 };
 </script>
